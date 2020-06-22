69e207e1722b03d615c59f6abe98a65a3cbe01f0
Keenah Vancampenhout <kvancamp@mail.com>
2020-06-22 21:32:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+
+def display_board(board=["   |   |   ", "------------", "   |   |   ", "------------", "   |   |   "])
+
+puts "#{board[0]}"
+puts "#{board[1]}"
+puts "#{board[2]}"
+puts "#{board[3]}"
+puts "#{board[4]}"
+
+
+end
+
+display_board
