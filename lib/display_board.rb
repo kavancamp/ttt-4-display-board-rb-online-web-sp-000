0c5239b8714c87b18d1820eac363a759f015cc02
Keenah Vancampenhout <kvancamp@mail.com>
2020-06-22 21:44:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,13 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 
+board = [" ", " ", " ", " ", "X ", " ", " ", " ", " "]
+ 
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board
